6d2afabcd3cde6b69413d0cdd8c5460fe5f04e44
safwanjmuhammed <safwanjmuhammed@gmail.com>
2025-07-26 19:18:54 +0530

    Ignored .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:iroid_application/src/app.dart';
+import 'package:iroid_application/src/config/app_config.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  loadEnv();
   runApp(ProviderScope(child: const MyApp()));
 }
+
+Future<void> loadEnv() async => await AppConfig.loadEnv();
--- lib/src/core/service/dio_api_service/api_service_interceptor.dart
+++ lib/src/core/service/dio_api_service/api_service_interceptor.dart
@@ -6,7 +6,7 @@ class DioServiceInterceptor extends Interceptor {
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    options.headers['Authorization'] = 'Bearer ${AppConfig.accessToken}';
+    options.headers['Authorization'] = 'Bearer ${AppConfig.token}';
     super.onRequest(options, handler);
   }
 
